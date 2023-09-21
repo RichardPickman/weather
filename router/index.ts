7a65522a52a78368c49924b8caf9efc0f5d42a76
RichardPickman <drezzerock@gmail.com>
2023-09-21 17:54:44 +0200

    rename middlewares

--- router/index.ts
+++ router/index.ts
@@ -1,11 +1,11 @@
 import e from "express";
 import { handleLogin } from "../controllers/user";
 import { handleWeather } from "../controllers/weather";
-import handleRequests from "../middlewares/requests";
-import JWTHandler from "../middlewares/token";
+import requestLimit from "../middlewares/requests";
+import jwtToken from "../middlewares/token";
 
 export const router = e();
 
-router.get("/weather/:city", JWTHandler, handleRequests, handleWeather);
+router.get("/weather/:city", jwtToken, requestLimit, handleWeather);
 
 router.post("/login", handleLogin);
