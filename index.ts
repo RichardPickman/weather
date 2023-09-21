067e359211bb000f42d551b5d741cc27227810f3
RichardPickman <drezzerock@gmail.com>
2023-09-21 19:16:40 +0200

    move app to own file

--- index.ts
+++ index.ts
@@ -1,13 +1,3 @@
-import e from "express";
-import cors from "cors";
-import error from "./middlewares/error";
-import { router } from "./router";
-
-const app = e();
-
-app.use(cors({ origin: "*" }));
-app.use(e.json());
-app.use(router);
-app.use(error);
+import app from "./app";
 
 app.listen(5000, () => console.log("Listening of port: ", 5000));
